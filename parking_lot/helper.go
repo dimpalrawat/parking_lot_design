46df30f5509365a00906620c06534f2d76eeee6c
Dimpal Rawat <dimpal.rawat@go-mmt.com>
2019-06-23 19:39:15 +0530

    Changing logic of checking if parking lot is already created or not

--- parking_lot/helper.go
+++ parking_lot/helper.go
@@ -69,73 +69,83 @@ func executeParkingLot(fileContentScanner *bufio.Scanner) bool {
 		RegToSlotNoMap: make(map[string]int),
 		VacatedSlots:   &VacatedSlotsHeap{},
 	}
-	commandNumber := 0
+	isInitialized := false
 	for fileContentScanner.Scan() {
-		commandNumber++
 		command := fileContentScanner.Text()
 		if command == "exit" {
 			break
 		}
-		parkingSlot.executeCommand(command, commandNumber)
-	}
-	if commandNumber == 0 {
-		return false
-	}
-	return true
-}
-
-func (this *ParkingLot) executeCommand (command string, commandNumber int) bool {
-	commandWords := strings.Split(command, " ")
-	if commandNumber == 1 {
-		if CommandMap[Command(commandWords[0])] == 1 {
-			slotSize := util.StringToInt(commandWords[1])
-			this.ParkingSlotSize = slotSize
-			this.VacatedSlots.InitializeHeap(slotSize)
-			this.BookedSlots = make([]*Vehicle, slotSize)
-			fmt.Println("Created a parking lot with " + commandWords[1] + " slots")
-		} else {
-			fmt.Println("Please initialize a parking lot first.")
-			return false
-		}
-	} else {
+		commandWords := strings.Split(command, " ")
 		switch CommandMap[Command(commandWords[0])] {
 		case 1:
+			if !isInitialized {
+				slotSize := util.StringToInt(commandWords[1])
+				parkingSlot.ParkingSlotSize = slotSize
+				parkingSlot.VacatedSlots.InitializeHeap(slotSize)
+				parkingSlot.BookedSlots = make([]*Vehicle, slotSize)
+				fmt.Println("Created a parking lot with " + commandWords[1] + " slots")
+				isInitialized = true
+				continue
+			}
 			fmt.Println("This program currently support only one active parking slot at a time.")
-			return false
 		case 2:
+			if !isInitialized {
+				fmt.Println("First step should be to create a parking lot.")
+				continue
+			}
 			vehicle := Vehicle{
 				Color:     commandWords[2],
 				RegNumber: commandWords[1],
 			}
-			slotNo := this.ParkVehicle(vehicle)
+			slotNo := parkingSlot.ParkVehicle(vehicle)
 			if slotNo == -1 {
 				fmt.Println("Sorry, parking lot is full")
 			} else {
 				fmt.Println("Allocated slot number: " + util.IntToString(slotNo))
 			}
 		case 3:
-			isVacated := this.VacateParkingSpot(util.StringToInt(commandWords[1]))
+			if !isInitialized {
+				fmt.Println("First step should be to create a parking lot.")
+				continue
+			}
+			isVacated := parkingSlot.VacateParkingSpot(util.StringToInt(commandWords[1]))
 			if isVacated {
 				fmt.Println("Slot number " + commandWords[1] + " is free")
 			} else {
 				fmt.Println("No vehicle is parked on slot: " + commandWords[1])
 			}
 		case 4:
-			fmt.Println(this.PrintStatus())
+			if !isInitialized {
+				fmt.Println("First step should be to create a parking lot.")
+				continue
+			}
+			fmt.Println(parkingSlot.PrintStatus())
 		case 5:
-			regNumbers := this.GetRegNosForColor(commandWords[1])
+			if !isInitialized {
+				fmt.Println("First step should be to create a parking lot.")
+				continue
+			}
+			regNumbers := parkingSlot.GetRegNosForColor(commandWords[1])
 			fmt.Println(regNumbers)
 		case 6:
-			slotNumber := this.GetSlotNoFromRegNo(commandWords[1])
+			if !isInitialized {
+				fmt.Println("First step should be to create a parking lot.")
+				continue
+			}
+			slotNumber := parkingSlot.GetSlotNoFromRegNo(commandWords[1])
 			fmt.Println(slotNumber)
 		case 7:
-			slotNumbers := this.GetSlotNosForColor(commandWords[1])
+			if !isInitialized {
+				fmt.Println("First step should be to create a parking lot.")
+				continue
+			}
+			slotNumbers := parkingSlot.GetSlotNosForColor(commandWords[1])
 			fmt.Println(slotNumbers)
 		default:
 			fmt.Println("Please enter a valid command")
 		}
 	}
-	return true
+	return isInitialized
 }
 
 //Function takes the vehicle object and returns slotNo if available else -1
