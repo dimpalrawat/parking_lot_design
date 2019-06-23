3c6b19a81b1e1a1278826f25cfb72b2441cb17ec
Dimpal Rawat <dimpal.rawat@go-mmt.com>
2019-06-23 21:29:31 +0530

    Adding test cases for get_slotno_from_regno functionality

--- parking_lot/helper_test.go
+++ parking_lot/helper_test.go
@@ -1,6 +1,10 @@
 package parking_lot
 
-import "testing"
+import (
+	"testing"
+	
+	"parking_lot_design/util"
+)
 
 func TestParkVehicle(t *testing.T) {
 	parkingLotSize := 2
@@ -67,5 +71,38 @@ func TestVacateParkingSpot(t *testing.T) {
 	isVacated = parkingLot.VacateParkingSpot(slot2)
 	if isVacated != false {
 		t.Errorf("Error VacateParkingSpot: Expected: %v Actual: %v", false, isVacated)
+	}
+}
+
+func TestGetSlotNoFromRegNo(t *testing.T) {
+	parkingLotSize := 2
+	parkingLot := ParkingLot{
+		RegToSlotNoMap:   make(map[string]int),
+		BookedSlots:      make([]*Vehicle, parkingLotSize),
+		VacatedSlots:     &VacatedSlotsHeap{},
+	}
+	parkingLot.ParkingSlotSize = parkingLotSize
+	parkingLot.VacatedSlots.InitializeHeap(parkingLotSize)
+	vehicle1 := Vehicle{
+		Color:     "White",
+		RegNumber: "KA-01-HH-1234",
+	}
+	slot1 := parkingLot.ParkVehicle(vehicle1)
+	vehicle2 := Vehicle{
+		Color:     "Black",
+		RegNumber: "KA-01-BB-0001",
+	}
+	slot2 := parkingLot.ParkVehicle(vehicle2)
+	slotNumber1 := util.StringToInt(parkingLot.GetSlotNoFromRegNo(vehicle1.RegNumber))
+	if slotNumber1 == 0 || slotNumber1 != slot1 {
+		t.Errorf("Error GetSlotNoFromRegNo: Expected: %v Actual: %v", slot1, slotNumber1)
+	}
+	slotNumber2 := util.StringToInt(parkingLot.GetSlotNoFromRegNo(vehicle2.RegNumber))
+	if slotNumber2 == 0 || slotNumber2 != slot2 {
+		t.Errorf("Error GetSlotNoFromRegNo: Expected: %v Actual: %v", slot2, slotNumber2)
+	}
+	slotNumber3 := util.StringToInt(parkingLot.GetSlotNoFromRegNo("KA-01-CC-3241"))
+	if slotNumber3 != 0 {
+		t.Errorf("Error GetSlotNoFromRegNo: Expected: %v Actual: %v", 0, slotNumber3)
 	}
 }
